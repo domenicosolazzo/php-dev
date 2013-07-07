9df9c39e097c8e97c20f94af22d66e94f461f0d5
Domenico Solazzo <solazzo.domenico@gmail.com>
2013-07-07 21:15:55 +0200

    Added apt_repository cmd for php54

--- metadata.rb
+++ metadata.rb
@@ -9,6 +9,16 @@ version          '0.1.2'
 
 depends "apache2"
 depends "apt"
+
+# Register PHP 5.4 PPA repository
+apt_repository "php54" do
+  uri "http://ppa.launchpad.net/ondrej/php5/ubuntu"
+  distribution node['lsb']['codename']
+  components ["main"]
+  keyserver "keyserver.ubuntu.com"
+  key "E5267A6C"
+end
+
 depends "build-essential"
 depends "mysql"
 depends "php"
--- recipes/default.rb
+++ recipes/default.rb
@@ -8,6 +8,16 @@
 #
 include_recipe "apt"
 
+
+# Register PHP 5.4 PPA repository
+apt_repository "php54" do
+  uri "http://ppa.launchpad.net/ondrej/php5/ubuntu"
+  distribution node['lsb']['codename']
+  components ["main"]
+  keyserver "keyserver.ubuntu.com"
+  key "E5267A6C"
+end
+
 include_recipe "apache2"
 
 include_recipe "build-essential"
