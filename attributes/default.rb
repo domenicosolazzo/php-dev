06a11f98e849b258c40d8af6baa2b5babfc340d0
Domenico Solazzo <solazzo.domenico@gmail.com>
2013-07-21 09:42:07 +0200

    Added attribute

--- attributes/default.rb
+++ attributes/default.rb
@@ -2,3 +2,4 @@ default[:app][:server_name]        = "localhost"
 default[:app][:server_aliases]     = ["*.localhost"]
 default[:app][:docroot]            = "/home/vagrant/web-app"
 default[:app][:php_timezone]       = "Europe/Rome"
+default[:apache2][:default_site_enabled] = false
