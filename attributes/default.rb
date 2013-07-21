9657bae9681b2b96466dbb25bc3bad7366d18025
Domenico Solazzo <solazzo.domenico@gmail.com>
2013-07-21 14:39:36 +0200

    Removing some attributes for apache

--- attributes/default.rb
+++ attributes/default.rb
@@ -5,17 +5,17 @@ default[:app][:php_timezone]       = "Europe/Rome"
 default[:app][:vhost_root]         = "/vagrant/sites/default"
 
 default[:apache][:default_site_enabled] = false
-default[:apache][:log_dir] = "/var/logs/apache2"
-default[:apache][:error_log] = "/var/logs/apache2/error"
-default[:apache][:access_log] = "/var/logs/apache2/access"
+#default[:apache][:log_dir] = "/var/logs/apache2"
+#default[:apache][:error_log] = "/var/logs/apache2/error"
+#default[:apache][:access_log] = "/var/logs/apache2/access"
 default[:apache][:user] = "vagrant"
 default[:apache][:group] = "vagrant"
 #default[:apache][:listen_ports] = 80
 default[:apache][:contact] = "admin@example.com"
-default[:apache][:timeout] = 300
-default[:apache][:keepalive] = "On"
-default[:apache][:keepaliverequests] = 100
-default[:apache][:keepalivetimeout] = 5
+#default[:apache][:timeout] = 300
+#default[:apache][:keepalive] = "On"
+#default[:apache][:keepaliverequests] = 100
+#default[:apache][:keepalivetimeout] = 5
 
 default[:apache][:prefork][:startservers] = 16
 default[:apache][:prefork][:minspareservers] = 16
